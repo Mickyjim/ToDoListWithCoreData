f9c17fdf071a90d57d743a10bc6a710aa39a4062
bastides <bastide.sebastien@gmail.com>
2019-08-02 17:07:10 +0200

    Setting up files architecture and Travis configuration

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-ToDoListWithCoreData
+# To do list with CoreData
+
+[![Build Status](https://travis-ci.org/bastides/ToDoListWithCoreData.svg?branch=master)](https://travis-ci.org/bastides/ToDoListWithCoreData)
+![GitHub](https://img.shields.io/github/license/bastides/ToDoListWithCoreData)
+
+Supports: iOS 12.x and above
+
+## Branches :
+
+* master - stable app releases
+* develop - development branch, merge your feature branches here
+
+### Description :
+
+To Do List to practice on CoreData. For iOS students (OpenClassRooms). 
---
+++ ToDoListWithCoreData/Sources/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  ToDoListWithCoreData
+//
+//  Created by Sebastien Bastide on 02/08/2019.
+//  Copyright © 2019 Sebastien Bastide. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
